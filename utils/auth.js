cc30bfce984a1fbf6a8c415380ceb9f065af02d4
Yonatan Perez <yonatan.perez.0213@gmail.com>
2018-07-24 00:36:36 -0700

    Include token to config and encrypt base64 (#2)

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 const inquirer = require('inquirer');
+const algorithm = 'aes-256-ctr';
 const homePath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 const configPath = path.join(homePath, '/.hiconfig');
+
+function encrypt(text, token){
+    var cipher = crypto.createCipher(algorithm, token);
+    var crypted = cipher.update(text,'utf8','hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function decrypt(text, token) {
+    var decipher = crypto.createDecipher(algorithm, token);
+    var dec = decipher.update(text,'hex','utf8');
+    dec += decipher.final('utf8');
+    return dec;
+}
+
+function createToken() {
+    var buf = crypto.randomBytes(128);
+    return buf.toString('hex');
+}
 
 const auth = {
     readAuth: function() {
@@ -45,12 +66,24 @@ const auth = {
 
             inquirer.createPromptModule()(questions)
             .then(({ username, password }) => {
-                let auth = new Buffer(username + ':' + password).toString('base64');
-                return resolve({ auth });
+                let token = createToken();
+                let base64 = new Buffer(username + ':' + password).toString('base64');
+                let auth = encrypt(base64, token);
+                return resolve({ auth, token });
             })
             .catch(reject);
         });
-    }
+    },
+    decryptAuth: function(config) {
+        return new Promise(function(resolve, reject) {
+            var token = config.token
+
+            if (!token)
+                return reject('Token is missing. Run `hi auth` to regenerate configuation file');
+            var auth = decrypt(config.auth, token);
+            return resolve({ auth, token });
+        });
+    },
 }
 
 module.exports = auth;
